69f3a729fcf3387f8b0e0ada2f0bb2c22bdb1204
AlquilerVehiculoProyecto <alquilerautosv@gmail.com>
2023-06-09 14:30:57 -0500

    personajemovimiento

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform charater;
+    private float sizeCamara;
+    private float heightScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sizeCamara = Camera.main.orthographicSize;
+        heightScreen = sizeCamara * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CalculateCameraPosition()
+    {
+        int characterScreen = (int)(charater.position.y / heightScreen);
+        float heightCamera = (characterScreen * heightScreen);
+    }
+}
